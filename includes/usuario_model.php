a65bdffc3ee5db268c5ad519d1ebe3ad429fec2e
Luis <lubilluse@gmail.com>
2025-06-11 19:56:03 -0500

    cargar usuarios en usuario.php y  creacion funcion modelo en  usuuariomodels.php

--- includes/usuario_model.php
+++ includes/usuario_model.php
@@ -14,5 +14,23 @@ function validarUsuario($conn, $usuario, $clave) {
     $stmt->close();
     return null;
 }
+function obtenerUsuarios($con) {
+    $stmt = $con->prepare("SELECT * FROM usuario");
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 0) {
+        $stmt->close();
+        return null; // No hay registros
+    }
+
+    $usuarios = [];
+    while ($fila = $result->fetch_assoc()) {
+        $usuarios[] = $fila;
+    }
+
+    $stmt->close();
+    return $usuarios; // Devuelve arreglo de usuarios
+}
 
 ?>
